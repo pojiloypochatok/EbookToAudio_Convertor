3f0219fe2186dd8762c413f4fddd689849dd543b
jackass oldman <82373905+Dedok228@users.noreply.github.com>
2025-05-22 00:32:26 +0300

    bug fixies and add gitignore

--- ebooktoaudio_manager.py
+++ ebooktoaudio_manager.py
@@ -1,35 +1,51 @@
-from torch.cuda.tunable import read_file
-
+from torch.cuda.tunable import read_file  # если используешь read_file из torch
 from utils.V2.extract_characters import split_into_chapters
 from utils.V2.tts_engine_V2 import generate_text
 import os
 import typing
 
 
-class BookManager():
-    def __init__(self, temp_path, output_path, input_path):
-        self.temp_path = temp_path,
-        self.output_path = output_path,
-        self.input_path = input_path
+def read_file(input_file):
+    # В будущем здесь планируется конвертация всех форматов
+    filename = os.path.splitext(os.path.basename(input_file))[0]
+    with open(input_file, "r", encoding="utf-8") as file:
+        content = file.read()
+        return content, filename
 
-    def read_file(self, input_file):
-        #В будущем здесь планирурется конвертация всех форматов
-        file_name = __name__(input_file)
-        with open(input_file) as file:
-            return file, file_name
+
+class BookManager:
+    def __init__(self, temp_path: str, output_path: str, input_path: str):
+        self.temp_path = temp_path
+        self.output_path = output_path
+        self.input_path = input_path
 
     def generate_with_characters(self, unsplit_text):
         if unsplit_text is not str:
             unsplit_text, file_name = read_file(unsplit_text)
             print(unsplit_text, file_name)
+
         characters = split_into_chapters(unsplit_text)
         for idx, character in enumerate(characters):
-            generate_text(character, f"{self.output_path}/{file_name}(character{idx})", self.temp_path)
+            generate_text(
+                character,
+                f"{self.output_path}/{file_name}(character{idx})",
+                self.temp_path
+            )
 
     def generate_without_characters(self, input_text):
         if input_text is not str:
             input_text, file_name = read_file(input_text)
             print(input_text, file_name)
-        generate_text(input_text, f"{self.output_path}/{file_name}", self.temp_path)
 
-ebooktoaudio = BookManager(output_path=r"output", temp_path=r"temp", input_path="input")
+        generate_text(
+            input_text,
+            f"{self.output_path}/{file_name}.wav",
+            self.temp_path
+        )
+
+
+ebooktoaudio = BookManager(
+    output_path="output",
+    temp_path="temp",
+    input_path="input"
+)
--- utils/V2/tts_engine_V2.py
+++ utils/V2/tts_engine_V2.py
@@ -20,7 +20,7 @@ model.to(device)
 
 
 # Создание ssml из текста
-def generate_ssml(text, pitch="low", rate="slow", strength="strong", volume=None):
+def generate_ssml(text, pitch="low", rate="very-slow", strength="strong", volume=None):
     prosody_attrs = f'pitch="{pitch}" rate="{rate}" strength="{strength}"'
     if volume:
         prosody_attrs += f' volume="{volume}"'
@@ -38,12 +38,12 @@ def split_text_by_length(text, max_len=800):
 
 
 # Озвучка + склейка
-def synthesize_and_merge(text, temp_path):
+def synthesize_and_merge(text, absol_temp_path):
     chunks = split_text_by_length(text)
     temp_files = []
 
     for idx, chunk in enumerate(chunks):
-        temp_path = f"{temp_files}/chunk_{idx}.wav"
+        temp_path = f"{absol_temp_path}/chunk_{idx}.wav"
         model.save_wav(text=chunk,
                        speaker=speaker,
                        sample_rate=sample_rate,
@@ -53,16 +53,19 @@ def synthesize_and_merge(text, temp_path):
     final_audio = AudioSegment.empty()
     for path in temp_files:
         final_audio += AudioSegment.from_wav(path)
+
+    for path in temp_files:
         os.remove(path)
+
     return final_audio
 
 
 def generate_text(text, output_path, temp_path):
     try:
         result = synthesize_and_merge(text, temp_path)
-        result.export(output_path)
+        result.export(output_path, format="wav")
     finally:
-        return f"Аудиофайл успешно сгенерирован и находится в {output_path}"
+        print(f"Аудиофайл успешно сгенерирован и находится в {output_path}")
 
 
 
